7324582625f7152838e111af50a314313e03e8c0
Francesca <fpcantor@gmail.com>
2019-01-11 00:44:02 +0000

    Fixed path in resource title; readded path attribute

--- 0x0D-configuration_management/0-create_a_file.pp
+++ 0x0D-configuration_management/0-create_a_file.pp
@@ -1,5 +1,6 @@
 # Creates a file in /tmp/holberton
-file { 'tmp/holberton':
+file { '/tmp/holberton':
+  path    => '/tmp/holberton',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
