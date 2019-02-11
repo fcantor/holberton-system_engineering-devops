f7a6535f13f2b9171e6a274504e61dc0f2d7a1d5
Francesca <fpcantor@gmail.com>
2019-02-11 23:37:13 +0000

    Initial commit

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Extends task 1 to export data in the JSON format """
+from requests import get
+from sys import argv
+import json
+
+
+def to_json(employee_id):
+    """ Uses get to pull data and exports data in a JSON format """
+    users = get("https://jsonplaceholder.typicode.com/users/" +
+                employee_id).json()
+    todos = get("https://jsonplaceholder.typicode.com/todos?userId=" +
+                employee_id).json()
+    if not users or not todos:
+        return ("Not a valid JSON")
+    task_list = []
+    for task in todos:
+        task_dict = {}
+        task_dict['task'] = task.get('title')
+        task_dict['completed'] = task.get('completed')
+        task_dict['username'] = task.get('name')
+        task_list.append(task_dict)
+    data = {}
+    data[employee_id] = task_list
+    with open('{}.csv'.format(employee_id), 'w') as file:
+        file.write(json.dumps(data))
+
+
+if __name__ == "__main__":
+    if len(argv) > 1:
+        to_json(argv[1])
